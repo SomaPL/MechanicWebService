94825b4663e3bd422fbb781a80bf2b834aefa8f9
Jakub Kot <Jakub.Kot@microsoft.wsei.edu.pl>
2023-01-24 21:16:41 +0100

    Added tables

---
+++ MechanicWebService/Models/Car.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MechanicWebService.Models
+{
+    public class Car
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Model { get; set; }
+        [Required]
+        public string Mark { get; set; }
+        [Required]
+        public string VIN { get; set; }
+        [Required]
+        public string Year { get; set; }
+
+    }
+}
---
+++ MechanicWebService/Models/Mechanic.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MechanicWebService.Models
+{
+    public class Mechanic
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string EmployeeID { get; set; }
+        public string Experiance { get; set; }
+    }
+}
