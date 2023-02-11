5ef93dcbb48b458dda7efa69c5249874b6cf2ffd
Jakub Kot <Jakub.Kot@microsoft.wsei.edu.pl>
2023-02-11 22:30:03 +0100

    Changes

--- MechanicWebService/Data/AppDbContext.cs
+++ MechanicWebService/Data/AppDbContext.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace MechanicWebService.Data
 {
-    public class AppDbContext:IdentityDbContext
+    public class AppDbContext:DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
         public DbSet<Models.Car> Cars { get; set; } = default!;
