4587a5d6bf923cdb8ecdbfa2ad4cc9e4eb184cf2
Jakub Kot <Jakub.Kot@microsoft.wsei.edu.pl>
2023-02-14 17:18:28 +0100

    Added Controllers and Views

---
+++ MechanicWebService/Controllers/MechanicsController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MechanicWebService.Data;
+using MechanicWebService.Models;
+
+namespace MechanicWebService.Controllers
+{
+    public class MechanicsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public MechanicsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Mechanics
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.Mechanics.ToListAsync());
+        }
+
+        // GET: Mechanics/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Mechanics == null)
+            {
+                return NotFound();
+            }
+
+            var mechanic = await _context.Mechanics
+                .FirstOrDefaultAsync(m => m.MechanicId == id);
+            if (mechanic == null)
+            {
+                return NotFound();
+            }
+
+            return View(mechanic);
+        }
+
+        // GET: Mechanics/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Mechanics/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MechanicId,FirstName,LastName,Experiance,Specialization")] Mechanic mechanic)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(mechanic);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(mechanic);
+        }
+
+        // GET: Mechanics/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Mechanics == null)
+            {
+                return NotFound();
+            }
+
+            var mechanic = await _context.Mechanics.FindAsync(id);
+            if (mechanic == null)
+            {
+                return NotFound();
+            }
+            return View(mechanic);
+        }
+
+        // POST: Mechanics/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("MechanicId,FirstName,LastName,Experiance,Specialization")] Mechanic mechanic)
+        {
+            if (id != mechanic.MechanicId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(mechanic);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MechanicExists(mechanic.MechanicId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(mechanic);
+        }
+
+        // GET: Mechanics/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Mechanics == null)
+            {
+                return NotFound();
+            }
+
+            var mechanic = await _context.Mechanics
+                .FirstOrDefaultAsync(m => m.MechanicId == id);
+            if (mechanic == null)
+            {
+                return NotFound();
+            }
+
+            return View(mechanic);
+        }
+
+        // POST: Mechanics/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Mechanics == null)
+            {
+                return Problem("Entity set 'AppDbContext.Mechanics'  is null.");
+            }
+            var mechanic = await _context.Mechanics.FindAsync(id);
+            if (mechanic != null)
+            {
+                _context.Mechanics.Remove(mechanic);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MechanicExists(int id)
+        {
+          return _context.Mechanics.Any(e => e.MechanicId == id);
+        }
+    }
+}
